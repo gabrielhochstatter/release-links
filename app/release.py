26e1a40ef20bd76f37b2a30003c692626c6d774a
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-08-17 16:32:29 +0100

    added release blueprint

---
+++ app/release.py
@@ -0,0 +1,10 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from app.auth import login_required
+from app.db import get_db
+
+bp = Blueprint('release', __name__)
+
