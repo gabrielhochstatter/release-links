c862ed91c5558d60082a6065af6c6b823e773567
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-08-23 16:28:28 +0100

    implemented editing of existing releases, added tooltips to edit and delete buttons

--- app/release.py
+++ app/release.py
@@ -8,14 +8,14 @@ from app.db import get_db
 
 bp = Blueprint('release', __name__)
 
-def get_release(id):
+def get_release(id, check_owner=True):
     release = get_db().execute(
-        'SELECT r.id, title, artist, label, link_1, link_2, link_3'
-        ' FROM release r WHERE r.id = ?', (id,)
+        'SELECT * FROM release WHERE id = ?', (id,)
     ).fetchone()
 
     if release is None:
         abort(404, "This release does not exist!")
+
 
     return release
 
@@ -49,7 +49,7 @@ def create():
             db.execute(
                 'INSERT INTO release (title, artist, label, link_1, link_2, link_3, owner_id)'
                 ' VALUES (?, ?, ?, ?, ?, ?, ?)',
-                (title, artist, label, link_1, link_2, link_3, g.user['id'])
+                (title, artist, label, link_1, link_2, link_3, g.user['id'],)
             )
             db.commit()
             return redirect(url_for('release.index'))
@@ -62,3 +62,47 @@ def display(id):
     release = get_release(id)
 
     return render_template('release/display.html', release=release)
+
+
+@bp.route('/r/update/<int:id>', methods=('GET', 'POST'))
+def update(id):
+    release = get_release(id)
+
+    if release['owner_id'] != g.user['id']:
+        abort(403)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        artist = request.form['artist']
+        label = request.form['label']
+        link_1 = request.form['link_1']
+        link_2 = request.form['link_2']
+        link_3 = request.form['link_3']
+        error = None
+
+        if not title:
+            error = 'A title is required!'
+
+        if not artist:
+            error = 'An artist name is required!'
+
+        if error is not None:
+            flash(error)
+
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE release SET' 
+                ' title = ?,'
+                ' artist = ?,'
+                ' label = ?,'
+                ' link_1 = ?,'
+                ' link_2 = ?,'
+                ' link_3 = ?'
+                ' WHERE id = ?',
+                (title, artist, label, link_1, link_2, link_3, release['id'],)
+            )
+            db.commit()
+            return redirect(url_for('release.display', id=release['id'])) 
+    else:
+        return render_template('release/update.html', release=release)
